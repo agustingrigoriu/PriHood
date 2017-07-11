28b6bdffdef4cc9b6997a0f4d38da7e59168d3c2
Lucas Madrazo <madrazolucas@gmail.com>
2017-07-11 13:11:22 -0300

    Login basico de usuario residente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,25 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage
   ],
   providers: [
     StatusBar,
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class LoginService {
+  private restApiURL_Users = 'http://localhost:8080';
+
+  constructor(private http: Http){
+  }
+
+  postUserCredentials(): Observable<Response>{
+    var datos = JSON.stringify({usuario:'Lucas',password:'123'})
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(this.restApiURL_Users, datos,{headers:headers})
+    //Incompleto
+  }
+}  
