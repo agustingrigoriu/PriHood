a109ac00b4182194eb162a0df8278e91273f7597
Lucas Madrazo <madrazolucas@gmail.com>
2017-10-16 20:12:57 -0300

    UI datos de ofrecimiento de viaje

--- src/pages/carpooling/nuevoCarpooling/nuevoOfrecimiento/nuevoOfrecimiento.ts
+++ src/pages/carpooling/nuevoCarpooling/nuevoOfrecimiento/nuevoOfrecimiento.ts
@@ -8,6 +8,10 @@ import { NavController } from 'ionic-angular';
 
 export class NuevoOfrecimientoPage {
 
+  asientos: number = 1;
+  esRecurrente: boolean = false;
+  id_dia_semana: number = 1;
+
   constructor(public navCtrl: NavController) { }
 
   volver() {
