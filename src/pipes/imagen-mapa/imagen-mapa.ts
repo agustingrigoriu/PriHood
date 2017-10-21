2952f8b73458a0adeebb99008e9ac6fa7dccc90f
Pato Perez <pato12p@gmail.com>
2017-10-21 20:32:01 -0300

    Agregar pipe para generar mapa

---
+++ src/pipes/imagen-mapa/imagen-mapa.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { Trayecto } from '../../app/models/trayecto.model';
+
+@Pipe({
+  name: 'imagenMapa',
+})
+export class ImagenMapaPipe implements PipeTransform {
+  transform(trayectos: Trayecto[], ...args) {
+    const mapa = 'https://maps.googleapis.com/maps/api/staticmap';
+    const params = ['size=400x250', 'sensor=false'];
+    const path = [];
+
+    const origen = trayectos[0];
+    const destino = trayectos[trayectos.length - 1];
+
+    for (let trayecto of trayectos) {
+      path.push(`${trayecto.latitud},${trayecto.longitud}`);
+    }
+
+    params.push(`path=${encodeURIComponent(path.join('|'))}`);
+
+    params.push(`markers=label:A|color:red|${origen.latitud},${origen.longitud}`);
+    params.push(`markers=label:B|color:green|${destino.latitud},${destino.longitud}`);
+
+    return `${mapa}?${params.join('&')}`;
+  }
+}
---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ImagenMapaPipe } from './../pipes/imagen-mapa/imagen-mapa';
+@NgModule({
+	declarations: [ImagenMapaPipe],
+	imports: [],
+	exports: [ImagenMapaPipe]
+})
+export class PipesModule {}
