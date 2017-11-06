8c8fd901872d76176d6f16cf899bc8d7d36826a9
Lucas Madrazo <madrazolucas@gmail.com>
2017-11-05 22:06:52 -0300

    Validacion registro proveedor

--- src/pages/proveedores/registroProveedor/registroProveedor.ts
+++ src/pages/proveedores/registroProveedor/registroProveedor.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, NavParams, ToastController } from 'ionic-angular';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { ProveedorService } from '../proveedores.service';
 import { ProveedoresPage } from '../proveedores'
 
@@ -13,24 +15,31 @@ export class RegistroProveedorPage {
 
   private tipos_servicio: any[] = [];
 
-  proveedor = {
-    id_tipo_servicio: '',
-    avatar: 'assets/img/pruebas/proveedores/piscinas.png'
-  };
+  formProveedor: FormGroup;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public toastCtrl: ToastController,
     public alertCtrl: AlertController,
-    public ProveedorService: ProveedorService) {
+    public ProveedorService: ProveedorService,
+    public formBuilder: FormBuilder) {
+
+    this.formProveedor = formBuilder.group({
+      id_tipo_servicio: ['', Validators.compose([Validators.required])],
+      nombre: ['', Validators.compose([Validators.maxLength(20), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      telefono: ['', Validators.compose([Validators.maxLength(20), Validators.pattern('[0-9]*'), Validators.required])],
+      direccion: ['', Validators.compose([Validators.required])],
+      descripcion: ['', Validators.compose([Validators.pattern('[a-zA-Z]*'), Validators.required])],
+      avatar: ['']
+    });
   }
 
   volver() {
     this.navCtrl.pop();
   }
 
-  registrarProveedor(proveedor) {
-    this.ProveedorService.registrarProveedor(proveedor).then(response => {
+  registrarProveedor() {
+    this.ProveedorService.registrarProveedor(this.formProveedor.value).then(response => {
       if (response.error) {
         this.presentToast('No se pudo completar el registro del servicio.');
       } else {
@@ -40,26 +49,22 @@ export class RegistroProveedorPage {
     });
   }
 
-  uploadImage() {
-    //completar con alert
-  }
-
-  //hardcodeo solo para mostrar en presentacion
+  //hardcodeo solo para mostrar en presentacion y que va a quedar por siempre
   cambiarImagen(id_tipo_servicio) {
     switch (id_tipo_servicio) {
-      case '1': this.proveedor.avatar = 'assets/img/pruebas/proveedores/piscinas.png';
+      case '1': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/piscinas.png');
         break;
-      case '2': this.proveedor.avatar = 'assets/img/pruebas/proveedores/jardineria.png';
+      case '2': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/jardineria.png');
         break;
-      case '3': this.proveedor.avatar = 'assets/img/pruebas/proveedores/plomero.png';
+      case '3': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/plomero.png');
         break;
-      case '4': this.proveedor.avatar = 'assets/img/pruebas/proveedores/gasista.png';
+      case '4': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/gasista.png');
         break;
-      case '5': this.proveedor.avatar = 'assets/img/pruebas/proveedores/fumigacion.png';
+      case '5': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/fumigacion.png');
         break;
-      case '6': this.proveedor.avatar = 'assets/img/pruebas/proveedores/otros.png';
-        break; 
-      default: this.proveedor.avatar = 'assets/img/pruebas/proveedores/piscinas.png';
+      case '6': this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/otros.png');
+        break;
+      default: this.formProveedor.controls['avatar'].setValue('assets/img/pruebas/proveedores/piscinas.png');
         break;
     }
 
@@ -90,8 +95,8 @@ export class RegistroProveedorPage {
   }
 
   servicioSeleccionado(tipo_servicio) {
-    this.proveedor.id_tipo_servicio = tipo_servicio;
-    this.cambiarImagen(this.proveedor.id_tipo_servicio);
+    this.formProveedor.value.id_tipo_servicio = tipo_servicio;
+    this.cambiarImagen(tipo_servicio);
   }
 
   ngOnInit() {
